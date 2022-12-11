1284212017a7b4b5c765a36cb959aebf128cb95f
teejstroyer <titoriano@gmail.com>
2022-12-11 14:25:26 -0600

    remove undo directory

--- lua/options.lua
+++ lua/options.lua
@@ -32,7 +32,6 @@ vim.opt.softtabstop = 4
 vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.updatetime = 50
 vim.opt.wrap = false
