9bb1ec1fb059b6a7e6a788453beccb35bef7982c
Titoriano Broadus <titoriano@gmail.com>
2023-08-23 10:28:34 -0500

    update git ignore

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,12 @@ return { 'nvim-treesitter/nvim-treesitter',
     config = function()
         require 'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all"
-            ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "c_sharp" },
+            ensure_installed = { "c", "c_sharp", "javascript", "lua", "lua", "query", "rust", "typescript", "vim", "vimdoc" },
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+            --auto_install = true,
             highlight = {
                 -- `false` will disable the whole extension
                 enable = true,
