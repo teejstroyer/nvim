15d0bda37edd397f5dc42632dfcb1f028ee2ce8c
Titoriano Broadus <titoriano@gmail.com>
2025-04-02 21:33:32 -0500

    improve virtual diagnostics

--- init.lua
+++ init.lua
@@ -237,19 +237,49 @@ vim.opt.updatetime = 300 -- Decrease update time
 vim.opt.wrap = true
 
 vim.diagnostic.config({
-  virtual_text = {
-    enabled = true,
-    --current_line
-    severity = {
-      max = vim.diagnostic.severity.WARN,
-    },
-  },
-  virtual_lines = {
-    enabled = true,
-    severity = {
-      min = vim.diagnostic.severity.ERROR,
-    },
-  },
+  virtual_text = true,
+  virtual_lines = { current_line = true },
+  underline = true,
+  update_in_insert = false
+})
+
+local og_virt_text
+local og_virt_line
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'DiagnosticChanged' }, {
+  group = vim.api.nvim_create_augroup('diagnostic_only_virtlines', {}),
+  callback = function()
+    if og_virt_line == nil then
+      og_virt_line = vim.diagnostic.config().virtual_lines
+    end
+
+    -- ignore if virtual_lines.current_line is disabled
+    if not (og_virt_line and og_virt_line.current_line) then
+      if og_virt_text then
+        vim.diagnostic.config({ virtual_text = og_virt_text })
+        og_virt_text = nil
+      end
+      return
+    end
+
+    if og_virt_text == nil then
+      og_virt_text = vim.diagnostic.config().virtual_text
+    end
+
+    local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+    if vim.tbl_isempty(vim.diagnostic.get(0, { lnum = lnum })) then
+      vim.diagnostic.config({ virtual_text = og_virt_text })
+    else
+      vim.diagnostic.config({ virtual_text = false })
+    end
+  end
+})
+
+vim.api.nvim_create_autocmd('ModeChanged', {
+  group = vim.api.nvim_create_augroup('diagnostic_redraw', {}),
+  callback = function()
+    pcall(vim.diagnostic.show)
+  end
 })
 
 -- LaTex Configuration
