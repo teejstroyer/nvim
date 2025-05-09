6501e7dead33d94321909c0edde0c3ceca92b894
Titoriano Broadus <titoriano@gmail.com>
2025-05-08 22:14:20 -0400

    trimming

--- init.lua
+++ init.lua
@@ -14,50 +14,13 @@ if not vim.loop.fs_stat(pipepath) then
 end
 
 ------------------------------------------
+--- ROCKS.nvim install
+-- nvim -u NORC -c "source https://raw.githubusercontent.com/nvim-neorocks/rocks.nvim/master/installer.lua"
 ------------------------------------------
---Rocks.nvim install
-do
-  local install_location = vim.fs.joinpath(vim.fn.stdpath("data"), "rocks")
-  local rocks_config = { rocks_path = vim.fs.normalize(install_location), }
 
-  vim.g.rocks_nvim = rocks_config
-
-  local luarocks_path = {
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
-  }
-  package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
-  local luarocks_cpath = {
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
-  }
-  package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
-  vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim",
-    "*"))
-end
-
--- If rocks.nvim is not installed then install it!
-if not pcall(require, "rocks") then
-  local rocks_location = vim.fs.joinpath(vim.fn.stdpath("cache"), "rocks.nvim")
-
-  if not vim.uv.fs_stat(rocks_location) then
-    -- Pull down rocks.nvim
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/nvim-neorocks/rocks.nvim",
-      rocks_location,
-    })
-  end
-
-  -- If the clone was successful then source the bootstrapping script
-  assert(vim.v.shell_error == 0, "rocks.nvim installation failed. Try exiting and re-entering Neovim!")
-  vim.cmd.source(vim.fs.joinpath(rocks_location, "bootstrap.lua"))
-  vim.fn.delete(rocks_location, "rf")
-end
-
-------------------------------------------
+vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
+require("mason").setup()
+require("mason-lspconfig").setup()
 
 require('mini.ai').setup()
 require('mini.colors').setup()
@@ -113,66 +76,19 @@ require('blink.cmp').setup({
   signature = { enabled = true }
 })
 
-require("image").setup(
-  {
-    backend = "kitty",
-    kitty_method = "normal",
-    integrations = {
-      markdown = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = false,
-        floating_windows = true, --
-      }
+require("image").setup({
+  backend = "kitty",
+  kitty_method = "normal",
+  integrations = {
+    markdown = {
+      enabled = true,
+      clear_in_insert_mode = false,
+      download_remote_images = true,
+      only_render_image_at_cursor = false,
+      floating_windows = true
     }
   }
-)
-
-require("quarto").setup({
-  lspFeatures = {
-    languages = { "r", "python", "rust" },
-    chunks = "all",
-    diagnostics = {
-      enabled = true,
-      triggers = { "BufWritePost" },
-    },
-    completion = {
-      enabled = false,
-    },
-  },
-  -- keymap = {
-  --   -- NOTE: setup your own keymaps:
-  --   hover = "H",
-  --   definition = "gd",
-  --   rename = "<leader>rn",
-  --   references = "gr",
-  --   format = "<leader>gf",
-  -- },
-  codeRunner = {
-    enabled = true,
-    default_method = "molten",
-  },
 })
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    require("quarto").activate()
-  end
-})
-require("jupytext").setup({
-  style = "markdown",
-  output_extension = "md",
-  force_ft = "markdown",
-})
-
--- REPL and Notebook requirements
-vim.g.molten_output_win_max_height = 20
-vim.g.molten_auto_open_output = false
-vim.g.molten_image_provider = "image.nvim"
-vim.g.molten_wrap_output = true
-vim.g.molten_virt_text_output = true
-vim.g.molten_virt_lines_off_by_1 = true
 
 --##############################################################################
 vim.g.have_nerd_font = true
@@ -182,8 +98,7 @@ vim.opt.colorcolumn = "120"
 vim.opt.completeopt = 'menuone,fuzzy,popup,noselect,preview'
 vim.opt.cursorline = true
 vim.opt.expandtab = true
-vim.opt.hlsearch = false  -- Set highlight on search
-vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.hlsearch = false -- Set highlight on search
 vim.opt.inccommand = 'split'
 vim.opt.incsearch = true
 vim.opt.isfname:append("@-@")
@@ -208,12 +123,65 @@ vim.opt.timeoutlen = 300
 vim.opt.undofile = true  -- Save undo history
 vim.opt.updatetime = 300 -- Decrease update time
 vim.opt.wrap = true
-
 vim.diagnostic.config({
   virtual_text = true,
   virtual_lines = { current_line = true },
   underline = true,
   update_in_insert = false
+})
+----------------------------------------------------
+-- LaTex Configuration
+----------------------------------------------------
+vim.g.tex_flavor = 'latex'                 -- Default tex file format
+vim.g.vimtex_compiler_progname = 'latexmk' -- Set compiler (optional)
+vim.g.vimtex_view_method = 'skim'          -- Set viewer (optional)
+vim.g.vimtex_view_skim_sync = 1
+vim.g.vimtex_view_skim_activate = 1
+vim.g.vimtex_continuous = 1               -- Enable continuous compilation
+vim.g.vimtex_view_automatic = 1           -- Automatically open PDF viewer
+vim.g.vimtex_snippets_enable_autoload = 1 -- Enable snippets
+vim.g.vimtex_compiler_latexmk = { out_dir = 'out' }
+
+----------------------------------------------------
+-- AUTO Commands
+----------------------------------------------------
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("HighlightYank", {}),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 40 })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("TrailingSpace", {}),
+  pattern = "*",
+  command = [[%s/\s\+$//e]], --removes trailing whitespace
+})
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  group = vim.api.nvim_create_augroup("TerminalLineNumbers", {}),
+  pattern = "*",
+  command = [[setlocal nonumber norelativenumber | startinsert]],
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          if _G.FormatOnSave then
+            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+          end
+        end
+      })
+    end
+  end
 })
 
 local og_virt_text
@@ -224,8 +192,6 @@ vim.api.nvim_create_autocmd({ 'CursorMoved', 'DiagnosticChanged' }, {
     if og_virt_line == nil then
       og_virt_line = vim.diagnostic.config().virtual_lines
     end
-
-    -- ignore if virtual_lines.current_line is disabled
     if not (og_virt_line and og_virt_line.current_line) then
       if og_virt_text then
         vim.diagnostic.config({ virtual_text = og_virt_text })
@@ -255,37 +221,6 @@ vim.api.nvim_create_autocmd('ModeChanged', {
   end
 })
 
--- LaTex Configuration
-vim.g.tex_flavor = 'latex'                 -- Default tex file format
-vim.g.vimtex_compiler_progname = 'latexmk' -- Set compiler (optional)
-vim.g.vimtex_view_method = 'skim'          -- Set viewer (optional)
-vim.g.vimtex_view_skim_sync = 1
-vim.g.vimtex_view_skim_activate = 1
-vim.g.vimtex_continuous = 1               -- Enable continuous compilation
-vim.g.vimtex_view_automatic = 1           -- Automatically open PDF viewer
-vim.g.vimtex_snippets_enable_autoload = 1 -- Enable snippets
-vim.g.vimtex_compiler_latexmk = { out_dir = 'out' }
-
---AUTO Commands
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("HighlightYank", {}),
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 40 })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("TrailingSpace", {}),
-  pattern = "*",
-  command = [[%s/\s\+$//e]], --removes trailing whitespace
-})
-
-vim.api.nvim_create_autocmd({ "TermOpen" }, {
-  group = vim.api.nvim_create_augroup("TerminalLineNumbers", {}),
-  pattern = "*",
-  command = [[setlocal nonumber norelativenumber | startinsert]],
-})
 
 ----------------------------------------------------
 -- FUNCTIONS
@@ -373,7 +308,6 @@ kmap("n", "<leader>tc",
     end)
   end, { desc = 'Toggle Conceal level' })
 
-
 kmap("n", "<leader>tf", function()
   _G.FormatOnSave = not _G.FormatOnSave
   vim.notify("Format on Save: " .. tostring(_G.FormatOnSave))
@@ -381,45 +315,7 @@ end, { desc = 'Toggle Format on Save' })
 
 -- Debug
 local dm = require("debugmaster")
-vim.keymap.set({ "n", "v" }, "<leader>d", dm.mode.toggle, { desc = "Debug Mode", nowait = true })
--- vim.keymap.set("t", "<C-/>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+kmap({ "n", "v" }, "<leader>d", dm.mode.toggle, { desc = "Debug Mode", nowait = true })
 
--- Molten Keymap
-kmap("n", "<localleader>e", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
-kmap("n", "<localleader>os", ":noautocmd MoltenEnterOutput<CR>", { desc = "open output window", silent = true })
-kmap("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { desc = "re-eval cell", silent = true })
-kmap("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "execute selection", silent = true })
-kmap("n", "<localleader>oh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
-kmap("n", "<localleader>md", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
-kmap("n", "<localleader>mx", ":MoltenOpenInBrowser<CR>", { desc = "open output in browser", silent = true })
-
-local runner = require("quarto.runner")
-kmap("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
-kmap("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
-kmap("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
-kmap("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
-kmap("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
-kmap("n", "<localleader>RA", function() runner.run_all(true) end, { desc = "run all cells of all languages", silent = true })
-
--- Image pasting
 local imgclip = require('img-clip')
 kmap("n", "<leader>p", imgclip.pasteImage, { desc = "Paste Image", silent = true })
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if not client:supports_method('textDocument/willSaveWaitUntil')
-        and client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-        buffer = args.buf,
-        callback = function()
-          if _G.FormatOnSave then
-            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-          end
-        end
-      })
-    end
-  end
-})
