fea20faf1e86241eaed896eca871a38a711c204d
Titoriano Broadus <titoriano@gmail.com>
2025-08-07 22:35:17 -0500

    educational vim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,3 +2,20 @@ vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter",
   "https://github.com/nvim-treesitter/nvim-treesitter-context",
 })
+
+---@diagnostic disable-next-line: missing-fields
+require("nvim-treesitter.configs").setup({
+  -- To add support for more languages, run `:TSInstall <language>`
+  -- or add the language to the `ensure_installed` list below.
+  ensure_installed = {
+    'lua', 'c_sharp'
+  },
+  auto_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
+})
