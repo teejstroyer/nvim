efdc19ad0f2217a2e89b3af83262179294b31e85
Titoriano Broadus <titoriano@gmail.com>
2025-12-05 15:25:58 -0600

    fix java junk

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -7,12 +7,4 @@ vim.pack.add({
 vim.pack.add({
   "https://github.com/nvim-neotest/neotest",
   --language support
-  'https://github.com/rcasia/neotest-java'
-})
-
----@diagnostic disable-next-line: missing-fields
-require("neotest").setup({
-  adapters = {
-    require("neotest-java")(),
-  },
 })
--- lua/plugins/quicktest.lua
+++ lua/plugins/quicktest.lua
@@ -1,4 +1,4 @@
-vim.pack.add({ "https://github.com/quolpr/quicktest.nvim" })
+vim.pack.add({ "https://github.com/MunifTanjim/nui.nvim", "https://github.com/quolpr/quicktest.nvim" })
 
 local qt = require("quicktest")
 
